fc1b3797b79a02795d6eaeaa72de25d26f34df8e
Zheleznichenko-Nina <zheleznicenkonina64@gmail.com>
2026-04-21 17:26:14 +0300

    fix invalid command

--- zheleznichenko.nina/T3/T3.cpp
+++ zheleznichenko.nina/T3/T3.cpp
@@ -16,25 +16,41 @@ struct Polygon {
   std::vector<Point> points;
 };
 
+void skipSpaces(std::istream& is) {
+  while (is.peek() == ' ' || is.peek() == '\t') {
+    is.ignore();
+  }
+}
+
+Point readPoint(std::istream& is) {
+  char open = 0, semi = 0, close = 0;
+  int x = 0, y = 0;
+
+  skipSpaces(is);
+  if (!(is >> open) || open != '(') { is.setstate(std::ios::failbit); return { 0,0 }; }
+  if (!(is >> x)) { is.setstate(std::ios::failbit); return { 0,0 }; }
+  skipSpaces(is);
+  if (!(is >> semi) || semi != ';') { is.setstate(std::ios::failbit); return { 0,0 }; }
+  if (!(is >> y)) { is.setstate(std::ios::failbit); return { 0,0 }; }
+  skipSpaces(is);
+  if (!(is >> close) || close != ')') { is.setstate(std::ios::failbit); return { 0,0 }; }
+
+  return Point{ x, y };
+}
+
 bool equalPoints(const Point& a, const Point& b) {
   return a.x == b.x && a.y == b.y;
 }
 
 bool pointLess(const Point& a, const Point& b) {
-  if (a.x < b.x) return true;
-  if (a.x == b.x) return a.y < b.y;
-  return false;
+  return (a.x < b.x) || (a.x == b.x && a.y < b.y);
 }
 
 bool isPerm(const Polygon& a, const Polygon& b) {
   if (a.points.size() != b.points.size()) return false;
-
-  std::vector<Point> pa = a.points;
-  std::vector<Point> pb = b.points;
-
+  std::vector<Point> pa = a.points, pb = b.points;
   std::sort(pa.begin(), pa.end(), pointLess);
   std::sort(pb.begin(), pb.end(), pointLess);
-
   return std::equal(pa.begin(), pa.end(), pb.begin(), equalPoints);
 }
 
@@ -49,31 +65,25 @@ bool isRightTriangle(const Polygon& p) {
     dotProduct(p.points[0], p.points[2], p.points[1]) == 0;
 }
 
-Point readPoint(std::istream& is) {
-  char open, semi, close;
-  int x, y;
-  if (!(is >> open >> x >> semi >> y >> close) || open != '(' || semi != ';' || close != ')') {
-    is.setstate(std::ios::failbit);
-    return Point{ 0, 0 };
-  }
-  return Point{ x, y };
-}
-
 Polygon parsePolygon(const std::string& line) {
+  if (line.empty()) throw std::runtime_error("empty");
   std::istringstream iss(line);
-  int n;
+  int n = 0;
   if (!(iss >> n) || n < 3) throw std::runtime_error("bad n");
 
   Polygon poly;
   std::generate_n(std::back_inserter(poly.points), n, std::bind(readPoint, std::ref(iss)));
 
-  if (iss.fail()) throw std::runtime_error("bad points");
+  if (iss.fail()) throw std::runtime_error("bad format");
+
+  std::string extra;
+  if (iss >> extra) throw std::runtime_error("extra data");
+
   return poly;
 }
 
 int main(int argc, char* argv[]) {
   if (argc < 2) return 1;
-
   std::ifstream file(argv[1]);
   if (!file) return 1;
 
@@ -81,9 +91,12 @@ int main(int argc, char* argv[]) {
   std::string line;
   while (std::getline(file, line)) {
     try {
-      if (!line.empty()) data.push_back(parsePolygon(line));
+      if (!line.empty()) {
+        data.push_back(parsePolygon(line));
+      }
     }
-    catch (...) {}
+    catch (...) {
+    }
   }
 
   std::string cmd;
@@ -93,7 +106,6 @@ int main(int argc, char* argv[]) {
         std::string rest;
         std::getline(std::cin, rest);
         Polygon target = parsePolygon(rest);
-
         std::cout << std::count_if(data.begin(), data.end(),
           std::bind(isPerm, std::placeholders::_1, target)) << "\n";
       }
@@ -102,6 +114,7 @@ int main(int argc, char* argv[]) {
       }
       else {
         std::cout << "<INVALID COMMAND>\n";
+        std::cin.ignore(10000, '\n');
       }
     }
     catch (...) {
